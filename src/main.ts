3037a9d5a010f31617b9179cefa43a002b6f6c27
Cesc342 <francescferrer1@gmail.com>
2020-12-05 12:28:40 +0100

    Problemes amb Token

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { coinBot } from "./bot";
 
 async function __main()
 {
-    await coinBot.prepararBot("NzE2MDA2NDE2NjczOTMxMzg1.XvDHKQ.nw6KUbfqL5jlycL5XBJyFoK5X10");
+    await coinBot.prepararBot("");
 }
 
 __main();
